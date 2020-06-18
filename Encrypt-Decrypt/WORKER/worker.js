d13070f9ffbb22e33a16dbbee4b9177b3c76b141
ri-dot <61711769+ri-dot@users.noreply.github.com>
2020-06-18 21:05:50 +0100

    New upload

--- Encrypt-Decrypt/WORKER/worker.js
+++ Encrypt-Decrypt/WORKER/worker.js
@@ -246,6 +246,10 @@ async function clone(data) {
   return result;
 }
 
+async function encrypt_(data) {
+  return encrypt(data.folder, KEY);
+}
+
 /**
  * Respond to the request
  * @param {Request} request
@@ -253,6 +257,85 @@ async function clone(data) {
 async function handleRequest(request) {
   if(request.method === "OPTIONS") {
     return new Response(null, {status: 200, headers: CORS_HEADERS});
+  }
+  else if(request.method === "GET" && request.url.endsWith("/encrypt")) {
+    return new Response(`<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Google Drive - encrypted folder copy</title>
+</head>
+<body>
+  <div class="container"><div class="row"><div class="col">
+    <div class="card my-4">
+      <div class="card-body">
+        <h3>Encrypt link</h3>
+        <div class="form-group form-row align-items-center">
+          <div class="col">
+            <input id="folder_link" type="text" class="form-control" placeholder="Google Drive folder link">
+          </div>
+          <div class="col-3 col-md-2">
+            <button id="encrypt" class="btn btn-primary btn-block">Encrypt</button>
+          </div>
+        </div>
+        <input id="encrypted_link" type="text" class="form-control" placeholder="encrypted ID">
+        <p class="mt-3">You can share the decryption website and encrypted ID separately or combine them together with <code>#</code> like this: <code>https://example.com/#ABCD.1234</code></p>
+      </div>
+      
+    </div>
+  </div></div></div>
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+
+async function encryptId(folderId) {
+  const result = await fetch("encrypt", {method: "POST", body: JSON.stringify({folder: folderId})});
+  const data = await result.json();
+  if(result.ok) {
+    return data.data;
+  }
+  else {
+    throw Error(data.reason);
+  }
+}
+
+async function encryptFolder() {
+  const link = document.querySelector("#folder_link").value;
+  const m = link.match(/^https:\\/\\/drive\\.google\\.com\\/(?:folderview\\?id=|open\\?id=|drive\\/(?:u\\/\\d+\\/)?folders\\/|file\\/(?:u\\/\\d+\\/)?d\\/)([0-9a-zA-Z\\-_]+)/);
+  if(m === null) {
+    throw Error("bad link!");
+  }
+
+  return encryptId(m[1]);
+}
+
+document.querySelector("#encrypt").addEventListener("click", async function(event) {
+  event.preventDefault();
+
+  document.querySelector("#folder_link").setAttribute("disabled", "");
+  document.querySelector("#encrypt").setAttribute("disabled", "");
+  try {
+    document.querySelector("#encrypted_link").value = "enltZS5nYS9FbmNyeXB0LURlY3J5cHQvaW5kZXguaHRtbDtnOnJpLWRvdC8zOTdmOWY2MzRmZmQ2MGE5NWViNjU5YzM1YWI4YTAwZTxUZXN0." + (await encryptFolder());
+  }
+  catch(e) {
+    alert(e.message);
+  }
+  finally {
+    document.querySelector("#folder_link").removeAttribute("disabled");
+    document.querySelector("#encrypt").removeAttribute("disabled");
+  }
+});
+
+  </script>
+</body>
+</html>
+`, {status: 200, headers: {"Content-Type": "text/html; charset=UTF-8"}});
   }
   else if(request.method !== "POST") {
     return fail("Method not allowed", 405);
@@ -264,6 +347,9 @@ async function handleRequest(request) {
   }
   else if(request.url.endsWith("/clone")) {
     parser = clone;
+  }
+  else if(request.url.endsWith("/encrypt")) {
+    parser = encrypt_;
   }
   else {
     return fail("Page not found", 404);
